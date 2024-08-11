61f804f2429b6ebe43548c7ae46483343dc6e925
chrisMJ <chris666.10@hotmail.com>
2024-08-11 17:52:27 -0500

    Controllers: productos

---
+++ controllers/productos.controller.php
@@ -0,0 +1,15 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER["REQUEST_METHOD"];
+if ($method == "OPTIONS") {
+    die();
+}
+
+// Controlador de productos
+require_once('../models/productos.model.php');
+error_reporting(0);
+$productos = new Productos;
+
